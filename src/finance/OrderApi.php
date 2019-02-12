50f644a9bd4d92428e4b1dde35899fb6f7ba20eb
黄龙 <huanglong@styd.cn>
2019-02-12 17:25:13 +0800

    init

---
+++ src/finance/OrderApi.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: huanglong
+ * Date: 2018/11/23
+ * Time: 下午3:11
+ */
+namespace styd\api\finance;
+
+class OrderApi {
+
+    public static function test(){
+        echo 'test';
+    }
+}
